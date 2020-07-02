c10e774d441ccdf2359662b96a1205e483806541
Aleah Connley <aleahkc@gmail.com>
2020-07-02 18:18:38 +0200

    page loader and link created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function executeWhenLoaded() {
+  alert("DOM is Loaded");
+}
 
+//Short hand:
+// $(function() {
+//     alert("DOM is Loaded");
+//   });
+
+$(document).ready(executeWhenLoaded);
+
+$(document).ready(function () {
+  $("a").click(function (event) {
+    alert("Hi jQuery! I Love You!!");
+  });
+});
